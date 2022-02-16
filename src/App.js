71a4df39ec1136334d39781d8d8d8e68e7ac9583
Destiny Erhabor <destinyerhabor6@gmail.com>
2022-02-16 22:16:18 +0100

    auth with metamask

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Metamask from './auth/metamask';
+
+function App() {
+  return (
+    <div className="App">
+     <Metamask />
+    </div>
+  );
+}
+
+export default App;
+
+
